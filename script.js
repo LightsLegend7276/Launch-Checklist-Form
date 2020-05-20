98511cd6ad8f0008dad6b95064d7271e1a58edb4
Denis Duarte <duarte.denis11@gmail.com>
2020-05-19 20:05:14 -0400

    Validation Complete

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+function init() {
+
+   let form = document.querySelector("form");
+
+   form.addEventListener("submit", function(event) {
+      let pilotInput = document.querySelector("input[name=pilotName]");
+      let copilotInput = document.querySelector("input[name=copilotName]");
+      let fuelInput = document.querySelector("input[name=fuelLevel]");
+      let massInput = document.querySelector("input[name=cargoMass]");
+      let inputArray = [pilotInput, copilotInput, fuelInput, massInput];
+      for (let input of inputArray) {
+         if (input.value === "") {
+            alert("All fields are required!");
+            event.preventDefault();
+            break;
+         };
+      };
+      if (isNaN(fuelInput.value) || isNaN(massInput.value)) {
+         alert("Fuel Level and Cargo Mass must be numbers!");
+         event.preventDefault();
+      };
+
+   });
+
+
+
+
+};
+
+
+window.onload = init;
+
+
 // Write your JavaScript code here!
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
@@ -11,3 +44,4 @@
 </ol>
 <img src="${}">
 */
+
