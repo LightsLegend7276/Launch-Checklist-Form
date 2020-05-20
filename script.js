e054de3f9c952e2c0cd00a4a2be9040bd8c709ea
Denis Duarte <duarte.denis11@gmail.com>
2020-05-19 22:26:55 -0400

    All done with Bonus

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function init() {
 
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then(function(json) {
+         let div = document.getElementById("missionTarget");
+         let planet = randomPlanet(json);
+         div.innerHTML = `
+         <h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${json[planet].name}</li>
+               <li>Diameter: ${json[planet].diameter}</li>
+               <li>Star: ${json[planet].star}</li>
+               <li>Distance from Earth: ${json[planet].distance}</li>
+               <li>Number of Moons: ${json[planet].moons}</li>
+            </ol>
+         <img src="${json[planet].image}">
+         `;
+      });
+   });
+
    let form = document.querySelector("form");
+   form.setAttribute("action", "#");
 
    form.addEventListener("submit", function(event) {
       let pilotInput = document.querySelector("input[name=pilotName]");
@@ -13,19 +32,36 @@ function init() {
             alert("All fields are required!");
             event.preventDefault();
             break;
-         };
+         }
       };
-      if (isNaN(fuelInput.value) || isNaN(massInput.value)) {
+      if (isNaN(fuelInput.value.toString()) || isNaN(massInput.value.toString())) {
          alert("Fuel Level and Cargo Mass must be numbers!");
          event.preventDefault();
-      };
+      } else {
+         let launchStatus = document.getElementById("launchStatusCheck");
+
+         launchStatus.innerHTML = `
+         ${fuelInput.value < 10000 || massInput.value > 10000 ? `<h2 id="launchStatus" style="color: red">Shuttle not ready for launch</h2>` : `<h2 id="launchStatus" style="color: green">Shuttle is ready for launch</h2>`}
+         <div>
+         <ol>
+         <li id="pilotStatus">${pilotInput.value} is ready for launch</li>
+         <li id="pilotStatus">${copilotInput.value} is ready for launch</li>
+         ${fuelInput.value < 10000 ? `<li id="fuelStatus">Fuel level too low for launch</li>` : `<li id="fuelStatus">Fuel level high enough for launch</li>`}
+         ${massInput.value > 10000 ? `<li id="cargoStatus">Cargo mass too high for launch</li>` : `<li id="cargoStatus">Cargo mass low enough for launch</li>`}
+         </ol>
+         </div>
+         `;
+         event.preventDefault();
+      }
 
    });
 
-
-
-
 };
+
+function randomPlanet(array) {
+   let index = Math.floor(Math.random() * array.length);
+   return index;
+}
 
 
 window.onload = init;
